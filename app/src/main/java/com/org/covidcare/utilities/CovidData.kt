ef54fecb57896eceb1d68c513b4b3ef133733baf
Ishwari Sorthe <ishwaris@cybage.com>
2020-06-24 12:50:18 +0530

    Few small changes

--- app/src/main/java/com/org/covidcare/utilities/CovidData.kt
+++ app/src/main/java/com/org/covidcare/utilities/CovidData.kt
@@ -1,11 +1,7 @@
 package com.org.covidcare.utilities
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import com.org.covidcare.model.Count
-import com.org.covidcare.model.Country
 import com.org.covidcare.model.District
-import com.org.covidcare.model.State
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.*
@@ -17,17 +13,15 @@ import kotlin.collections.ArrayList
 object CovidData {
 
     val dataCount = ArrayList<Count>()
-    val countries = ArrayList<Country>()
-    val states = ArrayList<State>()
     val districts = ArrayList<District>()
     fun compareToDay(userDate: String?): Boolean {
         if (userDate == null) {
             return false
         }
         val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        val userDate = sdf.parse(userDate)
+        val dataDate = sdf.parse(userDate)
         val currentDate = sdf.parse((LocalDateTime.now()).toString())
-        if(userDate?.compareTo(currentDate)==0){
+        if(dataDate?.compareTo(currentDate)==0){
             return true
         }
         return false
