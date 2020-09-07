16113d973c052ebefd37b34ca0371896bd857205
aditya-shiradhonkar <unipol6>
2020-09-07 17:18:54 +0530

    Changes regarding Helpline section

--- app/src/main/java/com/org/covidcare/ui/WelcomeFragment.kt
+++ app/src/main/java/com/org/covidcare/ui/WelcomeFragment.kt
@@ -1,21 +1,28 @@
 package com.org.covidcare.ui
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.org.covidcare.R
 import com.org.covidcare.utilities.CovidData
 import kotlinx.android.synthetic.main.fragment_welcome.view.*
+import kotlinx.android.synthetic.main.login_dialog_layout.*
 
 /**
  * Created by ishwari s on 7/9/2020.
  */
 private const val USER_NAME = "userName"
 
-class WelcomeFragment : Fragment(),View.OnClickListener {
+class WelcomeFragment : Fragment(), View.OnClickListener {
+    private lateinit var dialog: Dialog
     private var userName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +52,7 @@ class WelcomeFragment : Fragment(),View.OnClickListener {
         )
         view.text_user_name.text = userName
         view.btn_logout.setOnClickListener(this)
+        view.btn_help_line.setOnClickListener(this)
         return view
     }
 
@@ -56,6 +64,22 @@ class WelcomeFragment : Fragment(),View.OnClickListener {
                 closeFragment()
                 CovidData.logout()
             }
+
+            R.id.btn_help_line -> {
+                showDialog(v);
+            }
+
+            R.id.btn_just_call -> {
+                //Toast.makeText(v.context, "Just a call", Toast.LENGTH_SHORT).show()
+                openFragment(HelplineFragment.newInstance())
+                dialog.dismiss()
+            }
+
+            R.id.btn_drop_query -> {
+                Toast.makeText(v.context, "Drop a query - Work in progress", Toast.LENGTH_SHORT).show()
+                //openFragment(QueryFragment.newInstance())
+                dialog.dismiss()
+            }
         }
     }
 
@@ -65,7 +89,20 @@ class WelcomeFragment : Fragment(),View.OnClickListener {
         transaction?.addToBackStack(null)
         transaction?.commit()
     }
+
     private fun closeFragment() {
         activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
+    }
+
+    private fun showDialog(view: View) {
+        dialog = Dialog(view.context)
+        dialog.setCancelable(true)
+        val dialogView: View =
+            activity!!.layoutInflater.inflate(R.layout.helpline_dialog_layout, null)
+        dialog.setContentView(dialogView)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialogView.findViewById<Button>(R.id.btn_just_call).setOnClickListener(this)
+        dialogView.findViewById<Button>(R.id.btn_drop_query).setOnClickListener(this)
+        dialog.show()
     }
 }
